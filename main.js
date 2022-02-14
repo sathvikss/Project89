89965357c33cd1c8739dd6acb2039ddb6e86546c
sathvikss <88003797+sathvikss@users.noreply.github.com>
2022-02-14 16:16:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function next(){
+    window.location="1index.html";
+    var o = document.getElementById("player1").value
+    var r = document.getElementById("player2").value
+    console.log(o+"is player 1")
+    console.log(r+"is player 2")
+}
